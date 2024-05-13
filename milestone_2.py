f8f36701fb20739c11076412737045e687a1e76b
Michael <h1m1w1@googlemail.com>
2024-05-14 00:38:34 +0100

    Hangman randomchoice commit

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,14 @@
+import random 
+
+
 # Step 1: Create a list containing the names of your 5 favorite fruits.
 favorite_fruits = ["car", "van", "lorry", "motorbike", "bus"]
 
 # Step 2: Assign this list to a variable called word_list.
 word_list = favorite_fruits
 
-# Step 3: Print out the newly created list to the standard output (screen).
-print(word_list)
+# Step 3: Generate a random word from the word_list.
+word = random.choice(word_list)
+
+# Step 4: Print out the newly created list to the standard output (screen).
+print(word)
